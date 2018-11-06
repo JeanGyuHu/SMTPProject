89ef1c70d996403e6520c7f737eb03d992ffdeec
Jean-Gyu Hu <gjwlsrb1700@gmail.com>
2018-11-06 11:38:53 +0900

    fix: 메시지 보내는 부분 코드 수정

--- src/Constants.java
+++ src/Constants.java
@@ -1,4 +1,10 @@
 
 public class Constants {
 	public final static int SMTP_PORT = 25;
+	public final static String SMTP_EHLO = "250";
+	public final static String SMTP_LOGIN = "334";
+	public final static String SMTP_FROM = "250";
+	public final static String SMTP_TO = "250";
+	public final static String SMTP_DATA = "354";
+	public final static String SMTP_QUIT = "250";
 }
--- src/SocketNetwork.java
+++ src/SocketNetwork.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -16,9 +18,11 @@ public class SocketNetwork {
 	private String data;
 	private String from;
 	private String to;
+	private String id;
+	private String password;
     private String[] temp;
     
-	public SocketNetwork(String data, String from, String to) throws Exception {
+	public SocketNetwork(String data, String from, String to,String password) throws Exception {
 	    localhost = InetAddress.getLocalHost();
 	    
 	    temp = from.split("@");
@@ -28,8 +32,24 @@ public class SocketNetwork {
 	    this.data = data;
 	    this.from = from;
 	    this.to = to;
+	    this.password = password;
 	}
 	
+    protected boolean sendCommand(String command, String param, String responseCode) throws IOException {
+        String response = "", code;
+       
+        out.println(command + param);
+        response = in.readLine();
+        
+        System.out.println(response);
+        code = response.substring(0,3);
+
+        if(!code.equals(responseCode))
+            return false;
+
+        return true;
+    }
+    
 	public boolean send() {
 	      try {
 			smtpPipe =(SSLSocket) ((SSLSocketFactory) SSLSocketFactory.getDefault())
@@ -41,36 +61,33 @@ public class SocketNetwork {
 		      in = new BufferedReader(new InputStreamReader(smtpPipe.getInputStream()));
 		      out = new PrintWriter(smtpPipe.getOutputStream(), true);
 		      
-		      String initialID = in.readLine();
-		      System.out.println(initialID);
+		      if(sendCommand("EHLO ", "smtp." + host, Constants.SMTP_EHLO)==false)
+		    	  return false;
 		      
-		      out.println("HELO " + localhost.getHostName());
-		      String welcome = in.readLine();
-		      System.out.println(welcome);
+		      id = Base64.getEncoder().encodeToString(temp[0].getBytes(StandardCharsets.UTF_8));
+		      password = Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
 		      
-		      out.println("MAIL From:<" + from + ">");
-		      String senderOK = in.readLine();
-		      System.out.println(senderOK);
 		      
-		      out.println("RCPT TO:<" + to + ">");
-		      String recipientOK = in.readLine();
-		      System.out.println(recipientOK);
-		      
-		      out.println("DATA");
-		      out.println(data);
+		      if(sendCommand("MAIL FROM: ", "<" + from + ">", Constants.SMTP_FROM)==false)
+		    	  return false;
+		      if(sendCommand("RCPT TO: ", "<" + to + ">" , Constants.SMTP_TO)==false)
+		    	  return false;
+		      if(sendCommand("DATA", "", Constants.SMTP_DATA)==false)
+		    	  return false;
 
-		      out.println(".");
-		      String acceptedOK = in.readLine();
-		      System.out.println(acceptedOK);
+		      if(sendCommand(".", "", Constants.SMTP_QUIT)==false)
+		    	  return false;
 		      
 		      out.println("QUIT");
-		      
-		      return true;
+		      in.close();
+		      out.close();
+		      smtpPipe.close();
 		      
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	      
 	      return true;
 	      
 	}
